ff470421cf497de4cd78806e5fabe99ebf8feed6
jackcrane <jack@jackcrane.rocks>
2023-01-23 20:09:12 -0600

    First push; launch workflows

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Utils, Typography } from "../ui-kit";
+
+const Home = () => {
+  return (
+    <Utils.Container>
+      <Typography.Text>Home</Typography.Text>
+    </Utils.Container>
+  );
+};
+
+export { Home };
---
+++ ui-kit/typography.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+const Text = styled.Text`
+  font-size: 20px;
+  color: #333;
+`;
+
+export { Text };
