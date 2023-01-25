70a355d2c731076113171a585c8b47a10414d0a8
jackcrane <jack@jackcrane.rocks>
2023-01-25 12:18:09 -0600

    Incorporate for issue Add `react-native-toast-message` #2

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Toast from "react-native-toast-message";
 
 import { Home } from "./screens/Home";
 
@@ -8,11 +9,14 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Toast />
+    </>
   );
 }
 
